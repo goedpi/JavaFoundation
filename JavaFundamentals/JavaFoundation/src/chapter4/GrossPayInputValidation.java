9722b9f0bad33bfb80cf2e4f3b6c845e5c7786ca
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-15 14:12:27 +0200

    added retry limit

--- JavaFundamentals/JavaFoundation/src/chapter4/GrossPayInputValidation.java
+++ JavaFundamentals/JavaFoundation/src/chapter4/GrossPayInputValidation.java
@@ -6,15 +6,25 @@ public class GrossPayInputValidation {
     public static void main(String[] args) {
         int rate = 15; 
         int maxhours = 40; 
+        int retryCount = 0; 
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many hours have you worked?");
 
         double hoursworked = scanner.nextDouble(); 
         //validate input
-        while (hoursworked > maxhours || hoursworked < 1) {
+        while (hoursworked > maxhours || hoursworked < 1 && retryCount < 5) {
             System.out.println("Error: Working hours must be between 1 and 40. Please try again");
+            retryCount++; 
+            //System.out.println("RetryCount = " + retryCount);
             hoursworked = scanner.nextDouble();
-          
+            
+            if (hoursworked < maxhours && hoursworked > 1) {
+                System.out.println("You're hours are correct and logged succesfully");
+                retryCount = 0; 
+            } else if (retryCount == 5) {
+                System.out.println("Maximum retries are reached. Program will be terminated.");
+                System.exit(0); 
+            }
         }
         double gross = rate * hoursworked; 
 
