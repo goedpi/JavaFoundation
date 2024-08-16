3186cfc428c213309b8babe70f0d074222e3d750
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-16 11:01:54 +0200

    improved dialog by adding roomnames to it

--- JavaFundamentals/JavaFoundation/src/chapter6/HouseAreaImproved.java
+++ JavaFundamentals/JavaFoundation/src/chapter6/HouseAreaImproved.java
@@ -8,16 +8,16 @@ public class HouseAreaImproved {
     public static void main(String[] args) {
         HouseAreaImproved calculator = new HouseAreaImproved();
         
-        Rectangle kitchen = calculator.getRoom(); 
-        Rectangle bathroom = calculator.getRoom();
+        Rectangle kitchen = calculator.getRoom("kitchen"); 
+        Rectangle bathroom = calculator.getRoom("bathroom");
         double area = calculator.calculateTotalArea(kitchen,bathroom);
         System.out.println("The total area is: " + area);
         calculator.scanner.close();
     }
-    public Rectangle getRoom(){
-        System.out.println("What's the length of room ");
+    public Rectangle getRoom(String roomname){
+        System.out.println("What's the length of room " + roomname);
         double length = scanner.nextDouble(); 
-        System.out.println("What's the width of room ");
+        System.out.println("What's the width of room " + roomname);
         double width = scanner.nextDouble(); 
 
         return new Rectangle(length, width);
