9db5be4520f61da079509d2091d35702e58118fa
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-16 10:35:46 +0200

    included prompt to calculate area

--- JavaFundamentals/JavaFoundation/src/chapter6/HousArea.java
+++ JavaFundamentals/JavaFoundation/src/chapter6/HousArea.java
@@ -1,5 +1,7 @@
 package chapter6;
 
+import java.util.Scanner;
+
 public class HousArea {
     public static void main(String[] args) {
         Rectangle house1 = new Rectangle();
@@ -12,7 +14,19 @@ public class HousArea {
         double areaOfRoom2 = house2.calculateArea();
 
         System.out.println(areaOfRoom2);
+        collectHouseDetails();
 
+    }
 
+    public static void collectHouseDetails(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your houses length");
+        double houseLength = scanner.nextDouble();
+        System.out.println("What's your houses width");
+       
+        double houseWidth = scanner.nextDouble();
+        Rectangle promptedRectangle = new Rectangle(houseLength, houseWidth);
+
+        System.out.println("The area of your house is " + promptedRectangle.calculateArea());
     }
 }
