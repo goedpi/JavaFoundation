bcc5be276da699732d0cffe50faf95da4b477b1f
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-16 13:39:29 +0200

    added files for excersice 9a

---
+++ JavaFundamentals/JavaFoundation/src/chapter9/Person.java
@@ -0,0 +1,31 @@
+package chapter9;
+
+public class Person {
+    private String name; 
+    private int age; 
+    private String gender;
+ 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
